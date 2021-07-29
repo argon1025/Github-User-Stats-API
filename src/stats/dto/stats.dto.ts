ea7ab5b65aca30152f21d0bee12850daf2b46f27
MINJE-98 <jmj012100@gmail.com>
2021-07-29 22:29:16 +0900

    dto 제거

--- src/stats/dto/stats.dto.ts
+++
@@ -1,9 +0,0 @@
-export class Stats {
-  name: string;
-  totalPRs: number;
-  totalCommits: number;
-  totalIssues: number;
-  totalStars: number;
-  contributedTo: number;
-  //   rank: { level: string; score: number };
-}
