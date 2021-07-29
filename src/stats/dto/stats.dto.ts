6f9cc90093d1fad6260840058988d948b4de594b
조민제 <minje@minje-MacBookPro-3.local>
2021-07-29 21:51:28 +0900

    TIL-13 Stats dto 생성

---
+++ src/stats/dto/stats.dto.ts
@@ -0,0 +1,9 @@
+export class Stats {
+  name: string;
+  totalPRs: number;
+  totalCommits: number;
+  totalIssues: number;
+  totalStars: number;
+  contributedTo: number;
+  //   rank: { level: string; score: number };
+}
