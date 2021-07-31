c30d3e69a2af9a784eda6fc3c03d39eb416c230a
MINJE-98 <jmj012100@gmail.com>
2021-08-01 01:16:31 +0900

    TIL-15 TokenManagerService module 추가

--- src/top-languages/top-languages.module.ts
+++ src/top-languages/top-languages.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TopLanguagesService } from './top-languages.service';
 import { TopLanguagesController } from './top-languages.controller';
+import { TokenManagerService } from 'src/tokenManager/tokenManager.service';
 
 @Module({
   controllers: [TopLanguagesController],
-  providers: [TopLanguagesService]
+  providers: [TopLanguagesService, TokenManagerService],
 })
 export class TopLanguagesModule {}
