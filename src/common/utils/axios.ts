6214995352ad589fb5c0f33db12d6c6752bca52a
MINJE-98 <jmj012100@gmail.com>
2021-07-31 19:13:45 +0900

    TIL-13 오류 처리 추가

--- src/common/utils/axios.ts
+++ src/common/utils/axios.ts
@@ -1,57 +1,74 @@
+import { HttpException } from '@nestjs/common';
 import axios from 'axios';
 
 export function stats_fetcher(token, variables) {
-  return axios({
-    url: 'https://api.github.com/graphql',
-    method: 'post',
-    headers: {
-      Authorization: `bearer ${token}`,
-    },
-    data: {
-      query: `
-          query userInfo($login: String!) {
-            user(login: $login) {
-              name
-              login
-              contributionsCollection {
-                totalCommitContributions
-                restrictedContributionsCount
-              }
-              repositoriesContributedTo(first: 1, contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]) {
-                totalCount
-              }
-              pullRequests(first: 1) {
-                totalCount
-              }
-              issues(first: 1) {
-                totalCount
-              }
-              followers {
-                totalCount
-              }
-              repositories(first: 100, ownerAffiliations: OWNER, orderBy: {direction: DESC, field: STARGAZERS}) {
-                totalCount
-                nodes {
-                  stargazers {
+  try {
+    return axios({
+      url: 'https://api.github.com/graphql',
+      method: 'post',
+      headers: {
+        Authorization: `bearer ${token}`,
+      },
+      data: {
+        query: `
+              query userInfo($login: String!) {
+                user(login: $login) {
+                  name
+                  login
+                  contributionsCollection {
+                    totalCommitContributions
+                    restrictedContributionsCount
+                  }
+                  repositoriesContributedTo(first: 1, contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]) {
                     totalCount
+                  }
+                  pullRequests(first: 1) {
+                    totalCount
+                  }
+                  issues(first: 1) {
+                    totalCount
+                  }
+                  followers {
+                    totalCount
+                  }
+                  repositories(first: 100, ownerAffiliations: OWNER, orderBy: {direction: DESC, field: STARGAZERS}) {
+                    totalCount
+                    nodes {
+                      stargazers {
+                        totalCount
+                      }
+                    }
                   }
                 }
               }
-            }
-          }
-          `,
-      variables,
-    },
-  });
+              `,
+        variables,
+      },
+    });
+  } catch (error) {
+    // 200으로 출력되는 에러
+    if (error.response) {
+      throw new HttpException({ code: 'github.UNKOWN', message: error.message }, 400);
+    }
+  }
 }
-export function totalCommit_fetcher(token, username) {
-  return axios({
-    method: 'get',
-    url: `https://api.github.com/search/commits?q=author:${username}`,
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/vnd.github.cloak-preview',
-      Authorization: `bearer ${token}`,
-    },
-  });
+export async function totalCommit_fetcher(token, username) {
+  try {
+    return await axios({
+      method: 'get',
+      url: `https://api.github.com/search/commits?q=author:${username}`,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/vnd.github.cloak-preview',
+        Authorization: `bearer ${token}`,
+      },
+    });
+  } catch (error) {
+    if (error.response) {
+      if (error.response.status == 403) {
+        throw new HttpException({ code: 'github.SHORT_RATE_LIMITED', message: '깃허브에서 일시적으로 접근을 막았습니다. 재시도 해주세요.' }, 403);
+      }
+      throw new HttpException({ code: 'github.UNKOWN', message: error.message }, 400);
+    }
+  }
 }
