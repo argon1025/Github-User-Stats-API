3da192a56f0655705d4d95caccbc8dc6f6316b23
MINJE-98 <jmj012100@gmail.com>
2021-08-01 01:24:13 +0900

    TIL-14 TokenManager 모듈 추가

--- src/repo/repo.controller.ts
+++ src/repo/repo.controller.ts
@@ -1,21 +1,18 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { TokenManagerService } from 'src/tokenManager/tokenManager.service';
 import { RepoService } from './repo.service';
 
 @Controller('repo')
 export class RepoController {
-  constructor(private readonly repoService: RepoService) {}
+  constructor(private readonly repoService: RepoService, private readonly tokenManagerService: TokenManagerService) {}
 
   @Get(':reponame/user/:username')
   repoFetch(@Param('reponame') reponame, @Param('username') username) {
-    console.log('1');
-
-    return this.repoService.repoFetch(process.env.TOKEN, reponame, username);
+    return this.tokenManagerService.githubApiFetcher(reponame, username);
   }
 
   @Get(':username')
   allrepoFetch(@Param('reponame') reponame, @Param('username') username) {
-    console.log('2');
-
-    return this.repoService.allrepoFetch(process.env.TOKEN, username);
+    return this.tokenManagerService.githubApiFetcher(username, this.repoService.allrepoFetch);
   }
 }
