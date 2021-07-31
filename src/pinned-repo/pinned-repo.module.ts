e3c940e025af7b4c7d31f5d3092f17c40d405249
MINJE-98 <jmj012100@gmail.com>
2021-08-01 01:16:16 +0900

    TIL-16 TokenManagerService module 추가

--- src/pinned-repo/pinned-repo.module.ts
+++ src/pinned-repo/pinned-repo.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PinnedRepoService } from './pinned-repo.service';
 import { PinnedRepoController } from './pinned-repo.controller';
+import { TokenManagerService } from 'src/tokenManager/tokenManager.service';
 
 @Module({
   controllers: [PinnedRepoController],
-  providers: [PinnedRepoService]
+  providers: [PinnedRepoService, TokenManagerService],
 })
 export class PinnedRepoModule {}
