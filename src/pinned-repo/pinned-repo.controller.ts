033f17f4d2d27cad29d55ec5c6283ef304b73c2b
MINJE-98 <jmj012100@gmail.com>
2021-08-01 01:21:36 +0900

    TIL-16 TokenManager 모듈 추가

--- src/pinned-repo/pinned-repo.controller.ts
+++ src/pinned-repo/pinned-repo.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { TokenManagerService } from 'src/tokenManager/tokenManager.service';
 import { PinnedRepoService } from './pinned-repo.service';
 
 @Controller('pinned-repo')
 export class PinnedRepoController {
-  constructor(private readonly pinnedRepoService: PinnedRepoService) {}
+  constructor(private readonly pinnedRepoService: PinnedRepoService, private readonly tokenManagerService: TokenManagerService) {}
   @Get(':username')
   PinnedRepoFetch(@Param('username') username: string) {
-    return this.pinnedRepoService.PinnedRepoFetch(process.env.TOKEN, username);
+    return this.tokenManagerService.githubApiFetcher(username, this.pinnedRepoService.PinnedRepoFetch);
   }
 }
