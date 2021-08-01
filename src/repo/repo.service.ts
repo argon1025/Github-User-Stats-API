628fff3e0c2eddc02dc6c22d50f0931ff201eb4f
MINJE-98 <jmj012100@gmail.com>
2021-08-01 19:41:46 +0900

    TIL-22 axios api fetcher 함수 명 및 몇몇 애매한 변수명 수정하였습니다.

--- src/repo/repo.service.ts
+++ src/repo/repo.service.ts
@@ -1,16 +1,18 @@
 import { HttpException, Injectable } from '@nestjs/common';
-import { all_repo_fetcher, repo_fetcher } from 'src/Common/utils/repo.axios';
-import { pinnedRepo_fetcher } from 'src/Common/utils/pinnedRepo.axios';
+import { allRepoResponse } from 'src/common/utils/axios/repo/allRepoResponse';
+import { pinnedRepoResponse } from 'src/common/utils/axios/repo/pinnedRepoResponse';
+import { repoResponse } from 'src/common/utils/axios/repo/repoResponse';
+// import { allRepoResponse, pinnedRepoResponse, repoResponse } from 'src/common/utils/axios';
+
 import testData from './test_data';
 
 @Injectable()
 export class RepoService {
   async repoFetch(token, username, reponame) {
-    const result = await repo_fetcher(token, { login: username, repo: reponame });
-    const data = result.data.data;
-    console.log(result.data.errors);
-    if (!!result.data.errors) {
-      if (result.data.errors[0].type == 'RATE_LIMITED') {
+    const response = await repoResponse(token, { login: username, repo: reponame });
+    const data = response.data.data;
+    if (!!response.data.errors) {
+      if (response.data.errors[0].type == 'RATE_LIMITED') {
         throw new HttpException({ code: 'RATE_LIMITED', message: '해당토큰의 접근 가능한 횟수가 초과되었습니다.' }, 403);
       }
     }
@@ -36,10 +38,10 @@ export class RepoService {
   }
 
   async allrepoFetch(token, username) {
-    const result = await all_repo_fetcher(token, { login: username });
-    const data = result.data.data;
-    if (!!result.data.errors) {
-      if (result.data.errors[0].type == 'RATE_LIMITED') {
+    const response = await allRepoResponse(token, { login: username });
+    const data = response.data.data;
+    if (!!response.data.errors) {
+      if (response.data.errors[0].type == 'RATE_LIMITED') {
         throw new HttpException({ code: 'github.RATE_LIMITED', message: '해당토큰의 접근 가능한 횟수가 초과되었습니다.' }, 403);
       }
     }
@@ -49,18 +51,21 @@ export class RepoService {
     return data.user.repositories.nodes;
   }
 
-  async PinnedRepoFetch(token, username) {
+  async pinnedRepoFetch(token, username) {
     // DB꺼랑 실제 꺼랑 1:1 매칭 후 뿌려주기
-    const result = await pinnedRepo_fetcher(token, { login: username });
-    if (!!result.data.errors) {
-      if (result.data.errors[0].type == 'RATE_LIMITED') {
+    const response = await pinnedRepoResponse(token, { login: username });
+
+    const data = response.data.data;
+
+    if (!!response.data.errors) {
+      if (response.data.errors[0].type == 'RATE_LIMITED') {
         throw new HttpException({ code: 'RATE_LIMITED', message: '해당토큰의 접근 가능한 횟수가 초과되었습니다.' }, 403);
       }
     }
-    if (result.data.data.user == null) {
+    if (data.user == null) {
       throw new HttpException({ code: 'NOTFOUNDUSER', message: '해당 유저는 존재하지 않습니다.' }, 404);
     }
-    let pinnedItems = result.data.data.user.pinnedItems.nodes;
+    let pinnedItems = data.user.pinnedItems.nodes;
     pinnedItems.forEach((pinnedItem) => {
       testData.find((element) => (element.node_id == pinnedItem.id ? (pinnedItem['data'] = element.data) : 0));
     });
