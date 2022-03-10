fb75da3d108e64c0cad73925a2e9fbe1551bb10f
MINJE-98 <jmj012100@gmail.com>
2022-03-10 19:15:20 +0900

    edit: userStats 로직 구현

--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -1,11 +1,12 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { toplanguageResponse } from 'src/common/utils/axios/stats/toplanguageResponse';
-import { totalCommitResponse } from 'src/common/utils/axios/stats/totalCommitResponse';
-import { usersStatsResponse } from 'src/common/utils/axios/stats/usersStatsResponse';
+import { GithubFetchersService } from 'src/github-fetchers/github-fetchers.service';
+import { TokenManagerService } from 'src/tokenManager/tokenManager.service';
 
 @Injectable()
 export class StatsService {
-  async statsFetch(token, username) {
+  constructor(private readonly tokenManagerService: TokenManagerService, private readonly githubFetchersService: GithubFetchersService) {}
+  async userStats(username) {
     const stats = {
       name: '',
       totalPRs: 0,
@@ -14,40 +15,24 @@ export class StatsService {
       totalStars: 0,
       contributedTo: 0,
     };
-    const response = await usersStatsResponse(token, { login: username });
-    // const test_error = {
-    //   errors: [
-    //     {
-    //       type: 'RATE_LIMITED',
-    //       message: 'API rate limit exceeded for user ID test',
-    //     },
-    //   ],
-    // };
-    const data = response.data.data;
-    if (!!response.data.errors) {
-      if (response.data.errors[0].type == 'RATE_LIMITED') {
-        throw new HttpException({ code: 'RATE_LIMITED', message: '해당토큰의 접근 가능한 횟수가 초과되었습니다.' }, 403);
-      }
-    }
-    if (data.user == null) {
-      throw new HttpException({ code: 'NOT_FOUND_USER', message: '해당 유저는 존재하지 않습니다.' }, 404);
-    }
-    const user = data.user;
-    stats.name = user.name || user.login;
 
-    stats.totalIssues = user.issues.totalCount;
-
-    stats.totalCommits = user.contributionsCollection.totalCommitContributions;
-    const res = await totalCommitResponse(token, username);
-
-    stats.totalCommits += res.data.total_count;
-    stats.totalCommits += user.contributionsCollection.restrictedContributionsCount;
-
-    stats.totalPRs = user.pullRequests.totalCount;
-
-    stats.contributedTo = user.repositoriesContributedTo.totalCount;
-
-    stats.totalStars = user.repositories.nodes.reduce((prev, curr) => {
+    const userStatsResponse = await this.tokenManagerService.githubTokenSupplier(username, this.githubFetchersService.usersStatsFetcher);
+    const totalCommentResponse = await this.tokenManagerService.githubTokenSupplier(username, this.githubFetchersService.totalCommentFetcher);
+    const userInfo = userStatsResponse.data.user;
+    // name
+    stats.name = userInfo.name || userInfo.login;
+    //Issues
+    stats.totalIssues = userInfo.issues.totalCount;
+    // commit
+    stats.totalCommits = userInfo.contributionsCollection.totalCommitContributions;
+    stats.totalCommits += totalCommentResponse.total_count;
+    stats.totalCommits += userInfo.contributionsCollection.restrictedContributionsCount;
+    //PR
+    stats.totalPRs = userInfo.pullRequests.totalCount;
+    //contributedTo
+    stats.contributedTo = userInfo.repositoriesContributedTo.totalCount;
+    //stars
+    stats.totalStars = userInfo.repositories.nodes.reduce((prev, curr) => {
       return prev + curr.stargazers.totalCount;
     }, 0);
     return stats;
