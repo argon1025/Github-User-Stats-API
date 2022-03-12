307f0b75b79bae579fe5a47e016cdf9090d9641d
MINJE-98 <jmj012100@gmail.com>
2022-03-12 23:04:22 +0900

    edit: users를 변경된 gitstats-fetcher에 맞게 수정

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,11 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { TokenManagerService } from '../tokenManager/tokenManager.service';
 import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @ApiTags('Users')
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService, private readonly tokenManagerService: TokenManagerService) {}
-  @Get(':username/weekly-stats')
-  @ApiOperation({ summary: 'Request weekly-stats data' })
-  @ApiParam({
-    name: 'username',
-    type: 'string',
-    required: true,
-    description: 'Github UserName',
-  })
-  weeklystats(@Param('username') username: string) {
-    // return this.tokenManagerService.githubApiFetcher(username, this.usersService.weeklyStats);
-  }
+  constructor(private readonly usersService: UsersService) {}
 
   @Get(':username/events/page/:page')
   @ApiOperation({ summary: 'Request all events data' })
@@ -34,6 +22,6 @@ export class UsersController {
     description: 'page',
   })
   allEvents(@Param('username') username: string, @Param('page') page: string) {
-    // return this.tokenManagerService.githubApiFetcher(username, this.usersService.allEvents, page);
+    return this.usersService.allEvents(username, page);
   }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TokenManagerService } from 'src/tokenManager/tokenManager.service';
+import { GithubFetchersService } from 'src/github-fetchers/github-fetchers.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [UsersController],
-  providers: [UsersService, TokenManagerService],
+  providers: [UsersService, TokenManagerService, GithubFetchersService],
 })
 export class UsersModule {}
