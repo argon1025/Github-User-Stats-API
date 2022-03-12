2076b3b8080eb730663c22936d4e31e54e32f693
MINJE-98 <jmj012100@gmail.com>
2022-03-12 23:03:44 +0900

    edit: 사용하지 않는 메서드 제거

--- src/tokenManager/tokenManager.service.ts
+++ src/tokenManager/tokenManager.service.ts
@@ -6,8 +6,11 @@ export class TokenManagerService {
   private tokenListScope: number;
   private tokenList: string[] = [];
   private tokenEnvValName: string = 'GITHUB_TOKEN_';
+  public tokenLength: number;
+  public tryCount: number = 0;
   constructor(private configService: ConfigService) {
     this.LoadTokenDataFromEnv();
+    this.tokenLength = this.tokenList.length;
   }
 
   // 토큰 데이터가 존재하는지 유무
@@ -72,7 +75,7 @@ export class TokenManagerService {
   changeToken() {
     //토큰이 저장된 배열의 길이를 구한다
     const tokenListLength = this.tokenList.length - 1;
-
+    this.tryCount += 1;
     // 토큰 리스트에 데이터가 없을 경우
     if (tokenListLength <= -1) {
       Logger.error('Token data does not exist in Env, stop changeToken');
@@ -96,53 +99,5 @@ export class TokenManagerService {
       Logger.log(`TokenScope is Change+ -> ${this.getTokenScope()}`, 'CHANGE_TOKEN');
       return true;
     }
-  }
-
-  async githubTokenSupplier(username: string, fetcher: any, option?: any) {
-    // 유저 이름이 올바르지 않을 경우
-    if (!username) {
-      throw new HttpException({ code: 'tokenManager.githubApiFetcher.InvalidUsername', message: '올바르지 않은 유저이름 입니다' }, 401);
-    }
-    let retryCount: number = 1;
-    // 전체 토큰 리스트 길이 보다 재시도 횟수가 작을경우 반복한다
-    while (this.tokenList.length >= retryCount) {
-      // fetcher 시도
-      try {
-        let TOKEN = this.getToken();
-        const USER_NAME = username;
-        let result;
-        if (!!option) {
-          result = await fetcher(TOKEN, USER_NAME, option);
-        } else {
-          result = await fetcher(TOKEN, USER_NAME);
-        }
-
-        // 성공했을 경우 데이터 리턴
-        return result;
-      } catch (error) {
-        console.log('++++++++++++++++++++++++++++++++++++++');
-        console.log(error);
-        console.log('++++++++++++++++++++++++++++++++++++++');
-
-        let ERROR_CODE = error.response.code;
-        let ERROR_MESSAGE = error.response.message;
-        let ERROR_STATUS = error.status;
-
-        // 토큰의 접근 가능한 횟수가 초과되었을 경우
-        if (ERROR_STATUS === 403) {
-          // 토큰을 변경한다
-          if (!this.changeToken()) {
-            throw new HttpException({ code: 'tokenManager.githubApiFetcher.changeTokenfail', message: '토큰 변경에 실패했습니다' }, 500);
-          }
-
-          // 재시도 횟수 증가
-          retryCount++;
-        } else {
-          // 발생된 에러를 전달하고 마친다
-          throw new HttpException({ code: ERROR_CODE, message: ERROR_MESSAGE }, ERROR_STATUS);
-        }
-      }
-    }
-    throw new HttpException({ code: 'tokenManager.githubApiFetcher.RATE_LIMITED', message: '서비스 제공가능한 토큰이 모두 소진되었습니다' }, 500);
   }
 }
